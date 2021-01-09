b587815efa13dd119cdfbb2f779e425b0349b704
Beatriz <beatrizsallesss@gmail.com>
2021-01-09 15:10:19 -0300

    ✅ test:  Created EmailValidator returns false test.

--- tests/email-validator.spec.js
+++ tests/email-validator.spec.js
@@ -1,7 +1,7 @@
-
+const validator = require('validator')
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
@@ -10,5 +10,12 @@ describe('Email Validator', () => {
     const sut = new EmailValidator()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
+    expect(isEmailValid).toBe(false)
   })
 })

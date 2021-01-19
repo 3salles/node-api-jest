dd81d4c24a38ca9536c411a794ac27002985ad19
Beatriz <beatrizsallesss@gmail.com>
2021-01-19 20:52:29 -0300

    ✅ test: Created ensure bcrypt false return test.

--- tests/encrypter.spec.js
+++ tests/encrypter.spec.js
@@ -1,15 +1,22 @@
-const { TestScheduler } = require('jest')
-
+const bcrypt = require('bcrypt')
 class Encrypter {
-  async compare (password, hashedPassword) {
-    return true
+  async compare (value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 }
 
 describe('Encrypter', () => {
   test('Should return true is bcrypt returns true', async () => {
     const sut = new Encrypter()
-    const isValid = await sut.compare('any_password', 'hashed_password')
+    const isValid = await sut.compare('any_avalue', 'hashed_value')
     expect(isValid).toBe(true)
+  })
+
+  test('Should return false is bcrypt returns false', async () => {
+    const sut = new Encrypter()
+    bcrypt.isValid = false
+    const isValid = await sut.compare('any_value', 'hashed_value')
+    expect(isValid).toBe(false)
   })
 })

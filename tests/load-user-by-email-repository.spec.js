782e3ea825298971d9492bf46aeb2d938156aa3a
Beatriz <beatrizsallesss@gmail.com>
2021-01-21 19:11:27 -0300

    ✅ test: Should return an user if user is found with an id and password.

--- tests/load-user-by-email-repository.spec.js
+++ tests/load-user-by-email-repository.spec.js
@@ -6,7 +6,13 @@ class LoadUserByEmailRepository {
   }
 
   async load (email) {
-    const user = await this.userModel.findOne({ email })
+    const user = await this.userModel.findOne({
+      email
+    }, {
+      projection: {
+        password: 1
+      }
+    })
     return user
   }
 }
@@ -45,10 +51,13 @@ describe('LoadUserByEmailRepository', () => {
 
   test('Should return an user if user is found', async () => {
     const { sut, userModel } = makeSut()
-    await userModel.insertOne({
+    const fakeUser = await userModel.insertOne({
       email: 'valid_email@mail.com'
     })
     const user = await sut.load('valid_email@mail.com')
-    expect(user.email).toBe('valid_email@mail.com')
+    expect(user).toEqual({
+      _id: fakeUser.ops[0]._id,
+      password: fakeUser.ops[0].password
+    })
   })
 })

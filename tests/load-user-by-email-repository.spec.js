70b3dd7f391775a7483e6c336a899be45fe06681
Beatriz <beatrizsallesss@gmail.com>
2021-02-09 09:27:40 -0300

    ✅ test : Should throw if no userModel is provided.

--- tests/load-user-by-email-repository.spec.js
+++ tests/load-user-by-email-repository.spec.js
@@ -42,4 +42,10 @@ describe('LoadUserByEmailRepository', () => {
       password: fakeUser.ops[0].password
     })
   })
+
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@mail.com')
+    expect(promise).rejects.toThrow()
+  })
 })

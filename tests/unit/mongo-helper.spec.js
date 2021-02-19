744186134058e31316108a1bb7a6840192e81566
Beatriz <beatrizsallesss@gmail.com>
2021-02-19 11:28:44 -0300

    ♻️ refactor: Disconnected MongoDB after MongoHelper tests.

--- tests/unit/mongo-helper.spec.js
+++ tests/unit/mongo-helper.spec.js
@@ -1,9 +1,15 @@
-const MongoHelper = require('../../src/infra/repositories/helpers/mongo-helper')
+const sut = require('../../src/infra/repositories/helpers/mongo-helper')
 
 describe('Mongo Helper', () => {
-  test('Should reconnect when getDb() is invoked an client is disconnect', async () => {
-    const sut = MongoHelper
+  beforeAll(async () => {
     await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('Should reconnect when getDb() is invoked an client is disconnect', async () => {
     expect(sut.db).toBeTruthy()
     await sut.disconnect()
     expect(sut.db).toBeFalsy()

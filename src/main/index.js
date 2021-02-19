28832a3d9c02cac412d6759a558da7826a46c28d
Beatriz <beatrizsallesss@gmail.com>
2021-02-19 11:56:26 -0300

    🚧 chore: Connected to MongoDB before listen for app.

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,7 @@
-const app = require('./config/app')
+const MongoHelper = require('../../src/infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-app.listen(5858, () => console.log('🚀 Server Running on port 5858'))
+MongoHelper.connect(env.mongUrl).then(() => {
+  const app = require('./config/app')
+  app.listen(5858, () => console.log('🚀 Server Running on port 5858'))
+}).catch(console.error)

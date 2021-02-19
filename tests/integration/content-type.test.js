1e179fd8fbb2dbc6d7f378b7b5ba297bba828d9f
Beatriz <beatrizsallesss@gmail.com>
2021-02-19 11:05:25 -0300

    ✅ test: Should return XML content-type if forced.

--- tests/integration/content-type.test.js
+++ tests/integration/content-type.test.js
@@ -8,4 +8,12 @@ describe('Content-Type Middleware', () => {
     })
     await request(app).get('/test_content_type').expect('content-type', /json/)
   })
+
+  test('Should return XML content-type if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app).get('/test_content_type_xml').expect('content-type', /xml/)
+  })
 })

8f11573985ebbb1e7c2f279ca51fc8c97466fd8b
Beatriz <beatrizsallesss@gmail.com>
2021-02-19 12:34:28 -0300

    🚚 refactor: Moved domain folder to upper folder.

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,4 +1,4 @@
-const { MissingParamError, InvalidParamError } = require('../../../utils/errors')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
   constructor ({ loadUserByEmailRepository, updateAccessTokenRepository, encrypter, tokenGenerator } = {}) {
